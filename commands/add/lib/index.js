ec6d5763b4895960f5a0e05c4ab167be5c445f87
meixiang474 <1436098850@qq.com>
2022-11-17 23:37:43 +0800

    chore: update

--- commands/add/lib/index.js
+++ commands/add/lib/index.js
@@ -7,6 +7,7 @@ const { sync: pkgUpSync } = require("pkg-up");
 const fse = require("fs-extra");
 const glob = require("glob");
 const ejs = require("ejs");
+const semver = require("semver");
 const userHome = require("user-home");
 const Command = require("@icya-cli/command");
 const log = require("@icya-cli/log");
@@ -23,6 +24,17 @@ const PAGE_TEMPLATE = [
   },
 ];
 
+const SECTION_TEMPLATE = [
+  {
+    name: "Vue2代码片段",
+    npmName: "@icya-cli/template-section-vue",
+    version: "latest",
+  },
+];
+
+const ADD_MODE_SECTION = "section";
+const ADD_MODE_PAGE = "page";
+
 process.on("unhandledRejection", (e) => {});
 
 class AddCommand extends Command {
@@ -30,24 +42,73 @@ class AddCommand extends Command {
     // 获取 add 命令的初始化参数
   }
   async exec() {
+    // 1. 选择复用方式
+    this.addMode = (await this.getAddMode()).addMode;
+    if (this.addMode === ADD_MODE_SECTION) {
+      // 安装代码片段
+      await this.installSectionTemplate();
+    } else {
+      // 安装页面模板
+      await this.installPageTemplate();
+    }
+  }
+
+  async installSectionTemplate() {
+    // 1. 获取片段安装路径
+    this.dir = process.cwd();
+    // 2. 选择代码片段模板
+    this.sectionTemplate = await this.getTemplate(ADD_MODE_SECTION);
+    // 3. 安装代码片段模板
+    // 3.1 预检查 (目录重名问题)
+    await this.prepare(ADD_MODE_SECTION);
+    // 3.2 下载代码片段
+    await this.downloadTemplate(ADD_MODE_SECTION);
+    // todo 3-1 0:0
+  }
+
+  async installPageTemplate() {
     // 1. 获取页面安装路径
     this.dir = process.cwd();
     // 2. 选择页面模板
-    this.pageTemplate = await this.getPageTemplate();
+    this.pageTemplate = await this.getTemplate();
     // 3. 安装页面模板
     // 3.0 预检查 (目录重名问题)
-    await this.prepare();
+    await this.prepare(ADD_MODE_PAGE);
     // 3.1 下载页面模板到缓存目录
-    await this.downloadTemplate();
+    await this.downloadTemplate(ADD_MODE_PAGE);
     // 3.2 将页面模板拷贝至指定目录
     await this.installTemplate();
-    // 4. 合并页面模板依赖
-    // 5. 页面模板安装完成
   }
 
-  async prepare() {
+  getAddMode() {
+    return inquirer.prompt({
+      type: "list",
+      name: "addMode",
+      message: "请选择代码添加模式",
+      choices: [
+        {
+          name: "代码片段",
+          value: ADD_MODE_SECTION,
+        },
+        {
+          name: "页面模板",
+          value: ADD_MODE_PAGE,
+        },
+      ],
+    });
+  }
+
+  async prepare(addMode = ADD_MODE_PAGE) {
     // 生成最终拷贝路径
-    this.targetPath = path.resolve(this.dir, this.pageTemplate.pageName);
+    if (addMode === ADD_MODE_PAGE) {
+      this.targetPath = path.resolve(this.dir, this.pageTemplate.pageName);
+    } else {
+      this.targetPath = path.resolve(
+        this.dir,
+        "components",
+        this.sectionTemplate.sectionName
+      );
+    }
     if (await pathExists(this.targetPath)) {
       throw new Error("页面文件夹已经存在");
     }
@@ -177,11 +238,16 @@ class AddCommand extends Command {
         );
         if (duplicateDep) {
           log.verbose("查询到重复依赖", duplicateDep);
-          if (templateDep.value !== duplicateDep.value) {
+          const templateRange = semver
+            .validRange(templateDep.value)
+            .split("<")[1];
+          const targetRange = semver
+            .validRange(duplicateDep.value)
+            .split("<")[1];
+          if (templateRange !== targetRange) {
             log.warn(
               `${templateDep.key}冲突，${templateDep.value} => ${duplicateDep.value}`
             );
-            // todo 5.9 6:27
           }
         } else {
           log.verbose("查询到新依赖", templateDep);
@@ -215,84 +281,104 @@ class AddCommand extends Command {
     log.success("安装页面模板依赖成功");
   }
 
-  async downloadTemplate() {
+  async downloadTemplate(addMode = ADD_MODE_PAGE) {
+    // 模板名称
+    const name = addMode === ADD_MODE_PAGE ? "页面" : "片段";
     // 缓存文件夹
     const targetPath = path.resolve(userHome, ".icya-cli", "template");
     // 缓存路径
     const storeDir = path.resolve(targetPath, "node_modules");
-    const { npmName, version } = this.pageTemplate;
+    const { npmName, version } =
+      addMode === ADD_MODE_PAGE ? this.pageTemplate : this.sectionTemplate;
     // 构建Package对象
-    const pageTemplatePackage = new Package({
+    const templatePackage = new Package({
       targetPath,
       storeDir,
       packageName: npmName,
       packageVersion: version,
     });
     // 页面模板存在更新，不存在则下载
-    if (!(await pageTemplatePackage.exists())) {
-      const spinner = spinnerStart("正在下载页面模板...");
+    if (!(await templatePackage.exists())) {
+      const spinner = spinnerStart(`正在下载${name}模板...`);
       await sleep();
       try {
-        await pageTemplatePackage.install();
+        await templatePackage.install();
       } finally {
         spinner.stop(true);
-        if (await pageTemplatePackage.exists()) {
-          log.success("下载页面模板成功");
-          this.pageTemplatePackage = pageTemplatePackage;
+        if (await templatePackage.exists()) {
+          log.success(`下载${name}模板成功`);
+          if (addMode === ADD_MODE_PAGE) {
+            this.pageTemplatePackage = templatePackage;
+          } else {
+            this.sectionTemplatePackage = templatePackage;
+          }
         }
       }
     } else {
-      const spinner = spinnerStart("正在更新页面模板...");
+      const spinner = spinnerStart(`正在更新${name}模板...`);
       await sleep();
       try {
-        await pageTemplatePackage.update();
+        await templatePackage.update();
       } finally {
         spinner.stop(true);
-        if (await pageTemplatePackage.exists()) {
-          log.success("更新页面模板成功");
-          this.pageTemplatePackage = pageTemplatePackage;
+        if (await templatePackage.exists()) {
+          log.success(`更新${name}模板成功`);
+          if (addMode === ADD_MODE_PAGE) {
+            this.pageTemplatePackage = templatePackage;
+          } else {
+            this.sectionTemplatePackage = templatePackage;
+          }
         }
       }
     }
   }
 
-  async getPageTemplate() {
+  async getTemplate(addMode = ADD_MODE_PAGE) {
+    const name = addMode === ADD_MODE_PAGE ? "页面" : "代码片段";
+    const TEMPLATE =
+      addMode === ADD_MODE_PAGE ? PAGE_TEMPLATE : SECTION_TEMPLATE;
     const { pageTemplate: pageTemplateName } = await inquirer.prompt({
       type: "list",
       name: "pageTemplate",
-      message: "请选择页面模板",
-      choices: this.createChoices(),
+      message: `请选择${name}模板`,
+      choices: this.createChoices(addMode),
     });
     // 2.1 输入页面名称
-    const pageTemplate = PAGE_TEMPLATE.find(
+    const pageTemplate = TEMPLATE.find(
       (item) => item.npmName === pageTemplateName
     );
     if (!pageTemplate) {
-      throw new Error("页面模板不存在");
+      throw new Error(name + "模板不存在");
     }
     const { pageName } = await inquirer.prompt({
       type: "input",
       name: "pageName",
-      message: "请输入页面名称",
+      message: `请输入${name}名称`,
       default: "",
       validate: function (value) {
         const done = this.async();
         if (!value || !value.trim()) {
-          done("请输入页面名称");
+          done(`请输入${name}名称`);
           return;
         }
         done(null, true);
       },
     });
-    pageTemplate.pageName = pageName.trim();
+    pageTemplate[addMode === ADD_MODE_PAGE ? "pageName" : "sectionName"] =
+      pageName.trim();
     return pageTemplate;
   }
 
-  createChoices() {
-    return PAGE_TEMPLATE.map((item) => ({
-      name: item.name,
-      value: item.npmName,
-    }));
+  createChoices(addMode) {
+    return addMode === ADD_MODE_PAGE
+      ? PAGE_TEMPLATE.map((item) => ({
+          name: item.name,
+          value: item.npmName,
+        }))
+      : SECTION_TEMPLATE.map((item) => ({
+          name: item.name,
+          value: item.npmName,
+        }));
   }
 }
 
